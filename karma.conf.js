3aab31328dbefbe84dfa01512dcd5cd2c660711b
Jeeyun Lim <jeeyun.lim@gmail.com>
2016-11-16 14:57:05 -0800

    Copyright
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
 module.exports = function (config) {
 
   var appBase = 'dist/dev/app/';      // transpiled app JS files
