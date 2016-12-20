afe973f7ba77b749922f51fabe446ef537523b3a
Jeeyun Lim <jeeyun.lim@gmail.com>
2016-12-20 15:49:43 -0800

    [core] Upgrading angular version and cli version
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss']
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     constructor(private router: Router) {
