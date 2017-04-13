ed7578973678791423a00542ef49946f6c53cba3
Jeeyun Lim <jeeyun.lim@gmail.com>
2017-04-13 15:42:37 -0700

    [core] Updating the seed to 0.9.0
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -15,6 +16,7 @@ import { AboutComponent } from './about/about.component';
         HomeComponent
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         FormsModule,
         HttpModule,
