825b4382a511539ccbf653a89c2e16154c1f4410
Matt Hippely <mhippely@vmware.com>
2017-08-18 14:42:06 -0700

    [chore] Upgrade seed dependencies - Modified app code for changes in Clarity v0.10.x - Modified code for changes to Jasmine - modified configurations for changes to multiple deps
    `ng start`, `ng test`, and `ng e2e` all run as expected.
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
         frameworks: ['jasmine', '@angular/cli'],
         plugins: [
             require('karma-jasmine'),
-            require('karma-phantomjs-launcher'),
+            require('karma-chrome-launcher'),
             require('karma-mocha-reporter'),
             require('karma-remap-istanbul'),
             require('@angular/cli/plugins/karma')
@@ -27,6 +27,10 @@ module.exports = function (config) {
                 lcovonly: './coverage/coverage.lcov'
             }
         },
+        coverageIstanbulReporter: {
+            reports: [ 'html', 'lcovonly', 'text-summary' ],
+            fixWebpackSourcePaths: true
+        },
         angularCli: {
             config: './angular-cli.json',
             environment: 'dev'
@@ -38,7 +42,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['PhantomJS'],
+        browsers: ['ChromeHeadless'],
         singleRun: true
     });
 };
